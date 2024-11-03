9c6b695cd886150e5190e31402f3117bb5bf3525
jumagu <jmgg.gutierrez@gmail.com>
2024-11-03 00:59:20 -0500

    feat: add functionality - create a pokemon input

--- src/app/pokemons/components/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.ts
@@ -1,11 +1,27 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  input,
+  computed,
+  Component,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+import { SimplePokemon } from '@/pokemons/interfaces';
 
 @Component({
   selector: 'pokemon-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink],
   templateUrl: './pokemon-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokemonCardComponent {}
+export class PokemonCardComponent {
+  public pokemon = input.required<SimplePokemon>();
+
+  public readonly pokemonImg = computed(
+    () =>
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+        this.pokemon().id
+      }.png`
+  );
+}
